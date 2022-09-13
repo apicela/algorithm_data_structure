2b349c6e0e766b9d440582d1fe93737e9e62117f
jamilzin1 <jamilnetobr@gmail.com>
2022-09-13 10:17:56 -0300

    sum

--- Manipulacao de Dados/Jogo das Caixas.c
+++ Manipulacao de Dados/Jogo das Caixas.c
@@ -3,25 +3,31 @@
 int main()
 {
     FILE *file;
-    char userDocument[20];
+    char userDocument[400];
     int caixaValor[2][2];
-    int i,j,  N;
+    int i,j, max=0, N, soma=0, fileira, coluna;
 
-    //printf("Digite o nome do arquivo de entrada:");
-    //scanf("%s",  &userDocument);
-    //file = fopen(userDocument, "r");
-    file = fopen("teste.txt", "r");
+    printf("Digite o nome do arquivo de entrada: "); // "Enter the input file name"
+    scanf("%s", userDocument);
+
+    file = fopen(userDocument, "r");
     fscanf(file, "%d", &N);
-    printf("%d\n", N);
-
-        for(i=1;i<N;i++)
+    
+        for(i=0;i<N;i++)
         {
             for(j=1;j<i+1;j++)
             {
-                fscanf(file,"%d", &caixaValor[i][j]);
+                fscanf(file,"%d\n", &caixaValor[i][j]);
+                soma = soma+caixaValor[i][j];
+                if(soma>max)
+                {
+                   max = soma;
+                   fileira =i;
+                   coluna = j;
+                }
             }
         }
-
+    printf("Resposta: fileira %d, caixa %d\n", fileira, coluna); 
     fclose(file);
     return 0;
 }
