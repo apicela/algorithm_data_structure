3e9237821c83dd4e0c425a5018604159d0fac575
jamilzin1 <jamilnetobr@gmail.com>
2022-10-14 16:27:27 -0300

    need discover else if

--- knackSackProblem/knapSackProblem.c
+++ knackSackProblem/knapSackProblem.c
@@ -5,7 +5,7 @@ int main()
 {
     FILE *file;
     char userDocument[300];
-    int bags, slotsBags[100], items, itemPrice[100], itemWeight[100], choice[100][100], amountItems[100]; 
+    int bags=0, slotsBags[100], items=0, itemPrice[100], itemWeight[100], choice[100][100], amountItems[100], maxSlots=0, table[5000][5000]; 
     printf("Digite o nome do arquivo: \n"); // "Enter the input file name"
     scanf("%s", userDocument);
     file = fopen(userDocument, "r");
@@ -40,18 +40,34 @@ int main()
             }
         }
     
-    /*
-    if(amountItems[i]>slotsBags[i])
+        for(int i=0; i<bags; i++)
+        {
+            if(slotsBags[i]>maxSlots)
+            {
+                maxSlots = slotsBags[i];
+            }
+        }
+
+    // idea: create a dynamic program 
+    printf("max %d\n", maxSlots);
+    for(int i=0; i<items; i++)
     {
-        printf("Solução inviavel\n");
-    } */
-
-
-
-
-
-
-
+        for(int j=0;j<maxSlots; j++)
+        {
+            if(i==0 | j==0)
+            {
+                table[i][j]=0;
+            }
+            else if(itemWeight[i]<j)
+            {
+                table[i][j]=max(itemPrice[i]);
+            }
+            else
+            {
+                table[i][j]=table[i-1][j];
+            }
+        }
+    }
 
 
 
