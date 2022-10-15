04719453a863b4fc3dde50049d1bde971e8c852b
jamilzin1 <jamilnetobr@gmail.com>
2022-10-14 23:48:25 -0300

    what i am doing xd

--- knackSackProblem/knapSackProblem.c
+++ knackSackProblem/knapSackProblem.c
@@ -1,12 +1,26 @@
 #include <stdio.h>
 
+int max(int a, int b);
 
+int max(int a,int b)
+{
+    if(a<b)
+    {
+        return b;
+    }
+    else
+    {
+        return a;
+    }
+} 
 int main()
 {
     FILE *file;
     char userDocument[300];
-    int bags=0, slotsBags[100], items=0, itemPrice[100], itemWeight[100], choice[100][100], amountItems[100], maxSlots=0, table[5000][5000]; 
+    int bags=0, slotsBags[100], items=0, itemPrice[100], itemWeight[100], 
+    choice[100][100], amountItems[100], maxSlots=0, table[2000][1000]; 
     printf("Digite o nome do arquivo: \n"); // "Enter the input file name"
+    
     scanf("%s", userDocument);
     file = fopen(userDocument, "r");
     // read quantity bags
@@ -50,30 +64,35 @@ int main()
 
     // idea: create a dynamic program 
     printf("max %d\n", maxSlots);
-    for(int i=0; i<items; i++)
+    printf("items: %d\n",items);
+    
+    for(int i=0; i<=maxSlots+1; i++)
     {
-        for(int j=0;j<maxSlots; j++)
+        for(int j=0;j<=(items+1); j++)
         {
             if(i==0 | j==0)
             {
                 table[i][j]=0;
             }
-            else if(itemWeight[i]<j)
+            else if(itemWeight[i]<=j)
             {
-                table[i][j]=max(itemPrice[i]);
+                table[i][j]=max((itemPrice[i]+table[i-1][j-i]), table[i-1][j]);
             }
             else
             {
                 table[i][j]=table[i-1][j];
             }
         }
+    } 
+        for(int j=0;j<maxSlots; j++)
+        {
+for(int i=0; i<items; i++)
+    {
+        printf("%d%d - %d  ", i,j,table[i][j]);
+        }
+        printf("\n");
     }
-
-
-
-
-
-
+printf("%d",table[6][10]);
 
 
 }
