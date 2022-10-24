5e98e96d7cd59b97abda21fe248e3971946c6e19
jamilzin1 <jamilnetobr@gmail.com>
2022-10-24 02:39:14 -0300

    error when fact > 10

--- BigFatorialWithoutLibrarys/bigFatorial.c
+++ BigFatorialWithoutLibrarys/bigFatorial.c
@@ -3,13 +3,15 @@
 #include <stdio.h>
 int main()
 {
-    int fact = 11, incrementa, v[800], resultado=0,sobra=0,temp[800];
+    int fact =10, incrementa, v[800], resultado=0,sobra=0,temp[800];
     incrementa =0;
     int tamanho=0, count=0;  
+
     for( int i=0;i<=800;i++){
         v[i]=NULL;
         temp[i]=NULL;
     }
+
     temp[0]=2;
 
             for (int factValue=3; factValue<=fact;factValue++) // para cada fatorial. devemos multiplicar todos v[] existentes
@@ -18,30 +20,43 @@ int main()
                 {
                     tamanho++;
                 }
+                if (factValue>10)
+                {
+                    printf("");
+                }
                 incrementa=0;
                 for(int i=0; i<(tamanho+1);i++) // começa com 0
                 {
                     resultado = temp[i]*factValue;
+                        if(incrementa==1)
+                        {
+                            v[i]=resultado%10+v[i];
 
-                    if(incrementa==1)
-                    {
-                    v[i]=resultado%10+v[i];
-                    }
-                    else
-                    {
-                    v[i]=resultado%10;
-                    }
-
+                        }
+                        else
+                        {
+                            v[i]=resultado%10;
+                        }
+                                if(v[i]/10>0)
+                                {
+                                    resultado = resultado+v[i]/10;
+                                      v[i]=v[i]%10;
+                                }
                     resultado = (resultado-(resultado%10))/10;
+
+
                     if(resultado>0)
                     {
                         incrementa=1;
-                        temp[i+1]=v[i+1];
-                        v[i+1]=resultado;
+
                     }
+                                            temp[i+1]=v[i+1];
+
+                    v[i+1]=resultado;                    
                     temp[i]=v[i];
                 }
                 }
+
 
 for (int k=tamanho; k>=0; k--){
 
