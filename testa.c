c1d1b9a4332f2d38da9aa86414e3be7e8abe5a05
jamilzin1 <jamilnetobr@gmail.com>
2022-11-03 00:45:00 -0300

    segmentos

---
+++ testa.c
@@ -0,0 +1,17 @@
+      
+      int main()
+      {
+        int K = 1, contadorSegmento = 0;
+              int N=11;
+      int **segmento;
+
+      segmento = (int **)malloc(2 * sizeof(int *));
+
+      for (int i = 0; i < 2; i++)
+      {
+
+              segmento[i] = (int *) malloc (N * sizeof (int));;
+
+      }
+      printf("xd\n");
+      }
