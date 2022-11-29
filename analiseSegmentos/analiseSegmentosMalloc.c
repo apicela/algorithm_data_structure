b640d8169c6a0f891f3bceecb0965f0340613f94
jamilzin1 <jamilnetobr@gmail.com>
2022-11-29 01:04:03 -0300

    parte 2 do projeto

--- analiseSegmentos/analiseSegmentosMalloc.c
+++ analiseSegmentos/analiseSegmentosMalloc.c
@@ -18,6 +18,11 @@ typedef struct {
   int pontoMedio;
 } TipoItem;
 
+struct dadosTeste
+{
+  int pontoMedio;
+  int padrao;
+};
 typedef struct TipoCelula *TipoApontador;
 
 typedef struct TipoCelula {
@@ -47,67 +52,6 @@ void Insere(TipoItem x, TipoLista *Lista)
   Lista -> Ultimo -> Item = x;
   Lista -> Ultimo -> Prox = NULL;
 }
-/*
-void Retira(TipoApontador p, TipoLista *Lista, TipoItem *Item)
-{   ---   Obs.: o item a ser retirado e  o seguinte ao apontado por  p --- 
-  TipoApontador q;
-  if (Vazia(*Lista) || p == NULL || p -> Prox == NULL) 
-  { printf(" Erro   Lista vazia ou posi  c   a o n  a o existe\n");
-    return;
-  }
-  q = p -> Prox;
-  *Item = q -> Item;
-  p -> Prox = q -> Prox;
-  if (p -> Prox == NULL) Lista -> Ultimo = p;
-  free(q);
-}
-
-void Imprime(TipoLista Lista)
-{ TipoApontador Aux;
-  Aux = Lista.Primeiro -> Prox;
-  while (Aux != NULL) 
-    { printf("%d ", Aux -> Item.tipoSegmento);
-      Aux = Aux -> Prox;
-    }
-}
- //////////////////////////////////////////////////////////////////////////////////////////////////////////
-  ///////////////////////////////////ARMAZENANDO DADOS EM ALOCAÇAO DINAMICA//////////////////////////////////
-   //////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-    // Criaçao de um vetor alocado dinamicamente 2 x N
-  int **segmento;
-
-  segmento = (int **)malloc(2 * sizeof(int *));
-
-  for (int i = 0; i < 2; i++)
-  {
-    segmento[i] = (int *)malloc(N * sizeof(int));
-  }
-
-  for(int linha=0; linha<2;linha++) // preenche a matriz alocada dinamicamente na linha 75 e 76
-    {
-        for(int coluna=0; coluna<totalSegmentos; coluna++)
-        {
-            if(linha==0){
-            segmento[linha][coluna]= elemento[coluna].tipoSegmento; // preenche a 1* linha 
-            }                                                       // com o tipo de segmento
-            else
-            {
-            segmento[linha][coluna]= elemento[coluna].numElementos; // preenche a 2* linha
-            }                                                       // com a quantidade de 
-        }                                                       // elementos de mesmo segmento
-    } // element                                                // registrados sequencialmente
-
-    printf("Matriz:\n");
-        for(int linha=0; linha<2;linha++) // imprime a matriz
-    {
-        for(int coluna=0; coluna<totalSegmentos; coluna++)
-        {
-            printf("%d ", segmento[linha][coluna]);
-        }
-        printf("\n");
-    } *//////////////////////////////////////////////////////////////////////////////////////////////////
-
 
 
 
@@ -150,7 +94,7 @@ int main()
 {
   FILE *arquivoSegmentos;
   char nomeArquivo[49];
-  int N;
+  int N,L,first=(-1);
   // Łinhas 16 a 33 = Leitura do Arquivo
   printf("Digite o nome do arquivo: ");
    scanf("%s", nomeArquivo);
@@ -159,97 +103,123 @@ int main()
   {
     printf("Erro no arquivo.");
   }
-  fscanf(arquivoSegmentos, "%d", &N);
+  fscanf(arquivoSegmentos, "%d", &L);
+  struct dadosTeste armazenamento[L];
+  int contagem=0;
+  for(;contagem<L; contagem++) {
+        fscanf(arquivoSegmentos, "%d", &N);
 
-  // VARIAVEIS
-  TipoItem item[N];
-  int K = 0, count = 1, last,position=0, oldposition=0;
-  TipoLista lista;
-  //TipoApontador p;
-  int elementosVetor[N];
+        // VARIAVEIS
+        TipoItem item[N];
+        int K = 0, count = 1, last,position=0, oldposition=0;
+        TipoLista lista;
+        //TipoApontador p;
+        int elementosVetor[N];
 
-  for (int i = 0; i < N; i++)
-  {
-    fscanf(arquivoSegmentos, "%d ", &elementosVetor[i]);
+        for (int i = 0; i < N; i++)
+        {
+          fscanf(arquivoSegmentos, "%d ", &elementosVetor[i]);
+        }
+
+        // ve quantos segmentos há no arquivo
+        for (int i = 0; i < N; i++)
+        {
+          position++;
+          last = elementosVetor[i];
+          if ((elementosVetor[i + 1]) == last && (i+1)!=N)
+          {
+            ++count; // conta a quantidade de elementos repetidos sequencialmente
+          }
+          else
+          {
+            item[K].pontoMedio= (oldposition+position)/2;
+            oldposition = position;
+            item[K].valor = elementosVetor[i]; // após contar a sequencia de elementos, é salvo
+            item[K].numElementos = count;      // o valor do elemento e quantas vezes se repete
+            last = elementosVetor[i + 1];          // o novo elemento a ser comparado é o proximo
+            count = 1;  // reseta a quantidade de elementos contadas
+            K++; // contabiliza a quantidade de segmentos
+          }
+        
+        }
+
+      int totalSegmentos = K; 
+        // aloca dinamicamente um vetor para salvar os valores obtidos no arquivo
+        int *crescenteValor;
+        crescenteValor = (int *)malloc(K*sizeof(int));
+        
+        for(int i=0;i<K;i++) 
+        {
+          crescenteValor[i]=item[i].valor;
+        }
+        
+      ordenar(crescenteValor, K); // pega os valores obtidos e os ordena de forma crescente
+      K = deletarElementosDuplicados(crescenteValor, K); // deleta elementos repetidos
+
+          for(int i=0; i<totalSegmentos;i++) // percorre todos os segmentos
+          {
+              for(int j=1; j<=K;j++) // a cada vez percorrida do segmento, verifica em qual tipo de 
+              {                      // segmento, o elemento se atribui.
+                  if(item[i].valor==crescenteValor[j-1])
+                  {
+                      item[i].tipoSegmento = j;
+                  }
+              }
+          }
+        free(crescenteValor);
+        FLVazia(&lista);
+
+        /*Insere cada chave na lista */
+        for (int i = 0; i < totalSegmentos; i++)
+          { 
+            Insere(item[i], &lista);
+          }
+
+        // PADRAO: 1 3 2 3 1 
+        int padrao =0;
+        for(int i=0; i < totalSegmentos; i++)
+        {
+          if(item[i].valor==128){
+            armazenamento[contagem].pontoMedio= item[i].pontoMedio;
+            if(first==-1){
+              first=contagem;}
+          }
+          if(i+4<totalSegmentos){
+            if( item[i].tipoSegmento == 1 && item[i+1].tipoSegmento == 3
+                && item[i+2].tipoSegmento == 2 && item[i+3].tipoSegmento == 3 
+                && item[i+4].tipoSegmento == 1)
+              {
+                padrao =1;
+              }
+          }
+        }
+
+      armazenamento[contagem].padrao=padrao; // armazena se segue o padrao ou não
   }
-
-  // ve quantos segmentos há no arquivo
-  for (int i = 0; i < N; i++)
+  int totalPadrao=0;
+  for (int i =0; i<L;i++)
   {
-    position++;
-    last = elementosVetor[i];
-    if ((elementosVetor[i + 1]) == last && (i+1)!=N)
-    {
-      ++count; // conta a quantidade de elementos repetidos sequencialmente
+    totalPadrao=armazenamento[i].padrao+totalPadrao;
+    //printf("i:%d -%d - %d\n",i, armazenamento[i].padrao, armazenamento[i].pontoMedio);
+  }
+  //printf("%d / %d e %d\n", first,armazenamento[first].pontoMedio,armazenamento[L-1].pontoMedio);
+  float value = (float)armazenamento[first].pontoMedio/(float)armazenamento[L-1].pontoMedio;
+  //HORA DOS PRINTS
+  //printf("VALUE: %f", value);
+  if(totalPadrao<L*0.7)
+  {
+    printf("Resultado: Formato da pista nao estimado.");
+  }
+  else if(0.7<value && value <1.3){
+        printf("Resultado: Pista em linha reta.");
+  }
+    else if(value>1.3){
+      printf("Resultado: Curva a direita.");
     }
     else
     {
-      item[K].pontoMedio= (oldposition+position)/2;
-      oldposition = position;
-      item[K].valor = elementosVetor[i]; // após contar a sequencia de elementos, é salvo
-      item[K].numElementos = count;      // o valor do elemento e quantas vezes se repete
-      last = elementosVetor[i + 1];          // o novo elemento a ser comparado é o proximo
-      count = 1;  // reseta a quantidade de elementos contadas
-      K++; // contabiliza a quantidade de segmentos
+            printf("Resultado: Curva a esquerda.");
     }
-  
-  }
-
-int totalSegmentos = K; 
-  // aloca dinamicamente um vetor para salvar os valores obtidos no arquivo
-  int *crescenteValor;
-  crescenteValor = (int *)malloc(K*sizeof(int));
-  
-  for(int i=0;i<K;i++) 
-  {
-    crescenteValor[i]=item[i].valor;
-  }
-  
-ordenar(crescenteValor, K); // pega os valores obtidos e os ordena de forma crescente
-K = deletarElementosDuplicados(crescenteValor, K); // deleta elementos repetidos
-
-    for(int i=0; i<totalSegmentos;i++) // percorre todos os segmentos
-    {
-        for(int j=1; j<=K;j++) // a cada vez percorrida do segmento, verifica em qual tipo de 
-        {                      // segmento, o elemento se atribui.
-            if(item[i].valor==crescenteValor[j-1])
-            {
-                item[i].tipoSegmento = j;
-            }
-        }
-    }
-  free(crescenteValor);
-  FLVazia(&lista);
-
-  /*Insere cada chave na lista */
-  for (int i = 0; i < totalSegmentos; i++)
-    { 
-      Insere(item[i], &lista);
-    }
-
-  // PADRAO: 1 3 2 3 1 
-  int padrao =0;
-  for(int i=0; i < totalSegmentos; i++)
-  {
-    if(i+4<totalSegmentos){
-      if( item[i].tipoSegmento == 1 && item[i+1].tipoSegmento == 3
-          && item[i+2].tipoSegmento == 2 && item[i+3].tipoSegmento == 3 
-          && item[i+4].tipoSegmento == 1)
-        {
-          padrao =1;
-        }
-    }
-  }
-
-    if(padrao == 1)
-    {
-      printf("Resultado: Padrao encontrado.");
-    }
-      else
-      {
-        printf("Resultado: Padrao nao encontrado.");
-      } 
-
   fclose(arquivoSegmentos);
   return 0;
 }   
